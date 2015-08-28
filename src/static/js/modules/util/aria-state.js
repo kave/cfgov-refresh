2e268a97fa3d1218127cc105985a22570ebeef0a
Steven Bellamy <steven.bellamy@cfpb.gov>
2015-08-28 10:51:29 -0400

    Adding Aria state and config code

---
+++ src/static/js/modules/util/aria-state.js
@@ -0,0 +1,82 @@
+/* ==========================================================================
+   Aria State
+
+   Code convetions copied from the following with major modifications:
+
+   - https://github.com/IBM-Watson/a11y.js
+     Copyright (c) 2014 IBM
+   ========================================================================== */
+
+'use strict';
+
+var ariaStatesConfig = require( '../../config/aria-states-config' );
+var ariaState;
+
+
+// Private Properties
+
+var _statePrefix = 'is';
+var _ariaStatePrefix = 'aria-';
+
+
+// Private Methods
+
+function _defineProperty( state, element, object ) {
+  var _value = object[state] || false;
+  var _state = state;
+  var _element = element;
+
+  Object.defineProperty( object, state, {
+    enumerable:   true,
+    configurable: true,
+    get: function() {
+      return _value;
+    },
+    set: function( value ) {
+      _value = value;
+      ariaState.set( _state, _element, _value );
+    }
+  } );
+}
+
+function _validateState( state ) {
+  return Boolean( ariaStatesConfig[_ariaStatePrefix + state] );
+}
+
+
+// Public Methods
+
+ariaState = {
+
+  create: function create( state, element ) {
+    return this.define( state, element, {} );
+  },
+
+  define: function define( state, element, object ) {
+    if ( _validateState( state ) ) {
+      state = _statePrefix + state.charAt( 0 ).toUpperCase() +
+              state.substring( 1 );
+      _defineProperty( state, element, object );
+    }
+
+    return object;
+  },
+
+  get: function get( state, element ) {
+    return element.getAttribute( ariaStatesConfig[_ariaStatePrefix + state] );
+  },
+
+  set: function set( state, element, value ) {
+    state = state.substr( 0, 2 ) === 'is' && state.substr( 2 ) || state;
+    state = state.toLowerCase();
+    if ( _validateState( state ) ) {
+      state = _ariaStatePrefix + state;
+      element.setAttribute( state, value );
+    }
+
+    return element;
+  }
+
+};
+
+module.exports = ariaState;
